9785ab638584089eefba8f3febdfecd6be0f6e1f
AvneetLohiya <108623413+AvneetLohiya@users.noreply.github.com>
2022-07-03 23:09:00 +0530

    myapp

---
+++ src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+<div className='MAIN-SECTION-IN-MAIN-SCREEN'>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h1 className='main-h1'>
+              Deliver products across India
+            </h1>
+            <p class="Meesho" >
+              Meesho ensures quick and hassle-free delivery of all your products across India
+             
+            </p>
+          </div>
+          <div class="col1">
+            <div className='division-of-img-contairner'>
+              <div className='flex-image-centerin-dividion-con'>
+            <img src="https://supplier.meesho.com/images/icon-19.svg" alt=""class = "image1"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+           <p className='img-color'>
+           Lowest Shipping
+           <br/> Charges
+           </p>
+           </div>
+
+           <div className='division-of-img-contairner'>
+            <img src="https://supplier.meesho.com/images/icon-20.svg" alt=""class = "image1"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <p className='img-color'>
+           28000+ Pincode
+           <br></br>Served          </p>
+           </div>
+
+           <div className='division-of-img-contairner' style={{border:'none'}}> 
+            <img src="https://supplier.meesho.com/images/icon-15.svg" alt=""class = "image1"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <p className='img-color'>
+           0% Commission
+           </p>
+           </div>
+             
+            
+
+
+          </div>
+
+          
+        </div>
+        
+        
+        
+      </div>
+      </div>
+  );
+}
+
+export default App;
